cb9b99913330cea4db8465aae06d5b488c9ca9da
20181105879 <2605753145@qq.com>
2019-11-05 17:01:03 +0800

    543
    435

--- 20.swift/ViewController.swift
+++ 20.swift/ViewController.swift
@@ -9,6 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+ 
+    @IBOutlet weak var tfnumber1: UITextField!
+    
+    @IBOutlet weak var tfnumber2: UITextField!
+    
+   
+    @IBOutlet weak var tfoption: UITextField!
+    
+    @IBOutlet weak var sum: UITextField!
+    
+ 
+    
+   
+    @IBAction func jia(_ sender: Any) {
+
+    tfoption.text="+"
+        
+    }
+    @IBAction func jian(_ sender: Any) {
+
+        tfoption.text="-"
+        
+    }
+    
+    @IBAction func cheng(_ sender: Any) {
+        tfoption.text="*"
+        
+    }
+    
+    
+    @IBAction func cu(_ sender: Any) {
+        tfoption.text="/"
+
+    }
+    
+    
+    
+    
+    @IBAction func result(_ sender: Any) {
+        var a:Double=Double (tfnumber1.text!)!
+        let b=Double (tfnumber2.text!)!
+        let op:String=(tfoption.text)!
+        var c=0.0
+        switch op {
+        case "+" :
+         c=a+b
+            
+        case  "-":
+         c=a-b
+            
+        case  "/":
+         c=a/b
+            
+        case "*":
+         c=a*b
+            
+            
+        default:
+            break
+            
+        }
+        sum.text=String(c)
+        
+        
+        
+        
+        
+        
+       
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
